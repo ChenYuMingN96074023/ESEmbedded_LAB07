58f02130c88c41291037fec080a40ce961873168
ChenYuMingN96074023 <jimmy19960229@gmail.com>
2019-04-29 15:54:59 +0800

    123

--- 3_exti/main.c
+++ 3_exti/main.c
@@ -4,20 +4,40 @@
 
 int main(void)
 {
-	//RCC Enable GPIOA
-	??????
+	//RCC Enable GPIOA*********
+	SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTA));
 
-	//GPIO Configurations
-	??????
+	//GPIO Configurations********
+	//MODER led pin = 00 => General purpose input mode
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_1_BIT(0));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_0_BIT(0));
 
-	//RCC Enable SYSCFG
-	??????
+	//OT led pin = 0 => Output push-pull
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OTYPER_OFFSET, OTy_BIT(0));
 
-	//SYSCFG Configurations
-	??????
+	//OSPEEDR led pin = 00 => Low speed
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(0));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(0));
 
-	//EXTI Configurations
-	??????
+	//PUPDR led pin = 00 => No pull-up, pull-down
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(0));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(0));
+
+	//RCC Enable SYSCFG**************
+	SET_BIT(RCC_BASE + RCC_APB2ENR_OFFSET , SYSCFGEN_BIT); 
+
+	//SYSCFG Configurations***************
+    //0000 to select PA0
+    WRITE_BITS(SYSCFG_BASE + SYSCFG_EXTICR1_OFFSET , EXTI0_3_BIT ,EXTI0_0_BIT,0b0000);
+    
+	
+
+	//EXTI Configurations**************
+	//set interrupt mask register to "not mask"
+	SET_BIT(EXTI_BASE + EXTI_IMR_OFFSET , 0);
+    //set falling triger
+	SET_BIT(EXTI_BASE + EXTI_FTSR_OFFSET , 0);
+    
 
 	//NVIC Configurations
 	SET_BIT(NVIC_ISER_BASE + NVIC_ISERn_OFFSET(0), 6); //IRQ6
